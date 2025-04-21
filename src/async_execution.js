43f0b33bafa8bf58aa98470777212cf268924d7e
ngarimwenda@gmail.com <ngarimwenda@gmail.com>
2025-04-21 23:52:49 +0300

    cleanup async execution. Add missing pass directive. Add types and small fixes

--- src/async_execution.js
+++ src/async_execution.js
@@ -8,10 +8,12 @@ const apiKey = process.env.OPENAI_KEY;
 const client = new OpenAI({ apiKey });
 
 /**
- * @param {string} systemPrompt Task for the llm.
- * @returns                     A readable stream of an llm's response.
+ * @param {object} props
+ * @param {string} props.systemPrompt Task guidelines for the llm.
+ * @param {string} props.userMessage  User's tasks
+ * @returns                           A readable stream of an llm's response.
  */
-const llmStream = async (systemPrompt) => {
+const llmStream = async ({ systemPrompt, userMessage }) => {
     const protocolPrompt = Get_AsyncProtocol_System_Prompt();
 
     const response = await client.responses.create({
@@ -19,7 +21,8 @@ const llmStream = async (systemPrompt) => {
         input: [
             { role: "developer", content: protocolPrompt },
             { role: "developer", content: systemPrompt },
-            { role: "user", content: "What is the point of this repo?" },
+            { role: "user", content: userMessage },
+            { role: "user", content: "<pass />" },
         ],
         store: true,
         stream: true,
@@ -31,6 +34,12 @@ const llmStream = async (systemPrompt) => {
     return response.toReadableStream();
 };
 
+/**
+ * Waits for a "response.output_text.done" event and chunks the response
+ * adding it to the internal buffer
+ * @param {import("stream").TransformOptions} options
+ * @returns {Transform}
+ */
 const createReplyChunker = (options) => {
     return new Transform({
         ...options,
@@ -45,25 +54,38 @@ const createReplyChunker = (options) => {
     });
 };
 
+/**
+ * Logs each response
+ * adding it to the internal buffer
+ * @param {import("stream").StreamOptions} options
+ * @returns {Transform}
+ */
+const createLogger = (options) => {
+    return new PassThrough({ objectMode: true, ...options }).on(
+        "data",
+        (chunk) => {
+            console.log(JSON.stringify(JSON.parse(chunk), null, 2));
+        }
+    );
+};
+
+/**
+ * The assistant API is fairly verbose. It sends multiple "assistant" messages per turn
+ * When you read the response lazily, even if messages are in JSON, they are incorrectly appended and failing on the client.
+ * One way around the issue is to stream the response and wait for "response.output_text.done" events and chunk the response.
+ * This will be helpful especially especially when we expand the protocol.
+ * It will allow models to "think in process" and for us to execute tools as the llm streams.
+ * We will be able to invoke tools mid-stream and unlock more performance in our executor.
+ * I need to investigate this behavior further
+ */
 export const main = async () => {
     const systemPrompt = getRepoSysPrompt("sample/control-tower");
-
-    /**
-     * The assistant API is fairly verbose. It sends multiple "assistant" messages per turn
-     * When you read the response lazily, even if messages are in JSON, they are incorrectly appended and failing on the client.
-     * One way around the issue is to stream the response and wait for "response.output_text.done" events and chunk the response.
-     * This will be helpful especially especially when we expand the protocol.
-     * It will allow models to "think in process" and for us to execute tools as the llm streams.
-     * We will be able to invoke tools mid-stream and unlock more performance in our executor.
-     * I need to investigate this behavior further
-     */
-    const stream = await llmStream(systemPrompt);
+    const userMessage = process.argv[3] || "What is the point of this repo?";
+    const stream = await llmStream({ systemPrompt, userMessage });
     pipeline(
         stream,
         createReplyChunker({ objectMode: true }),
-        new PassThrough().on("data", (chunk) => {
-            console.log(chunk.toString());
-        }),
+        createLogger({ objectMode: true }),
         (err) => err && console.error({ err })
     );
 };
